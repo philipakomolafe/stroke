efe50bccb47da767741a7fc7cd4a2c7e70f41301
philip akomolafe <akomolafephilip@gmail.com>
2026-01-13 18:55:03 +0100

    Update on inference.py

--- inference.py
+++ inference.py
@@ -8,7 +8,6 @@ from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel, Field
 from typing import Any, Optional
 from sklearn.preprocessing import PowerTransformer
-import google.genai as gen
 
 
 
